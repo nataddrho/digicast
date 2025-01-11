73719e81ad9fb894a09330ee2be1b6445d521cb3
nataddrho <nataddrho@gmail.com>
2025-01-11 18:42:06 -0500

    Need to fix parsers

--- python/bluetooth_le.py
+++ python/bluetooth_le.py
@@ -6,7 +6,6 @@ class BLE_async():
 
     def __init__(self):
         self.test = False #Turn on to test graphical display
-        self._devices = None
         self._done = True
         self._last_shot_number = -1
         self._digiball_mac_addresses = [None, None]
@@ -75,82 +74,83 @@ class BLE_async():
 
         return digiball_data, digicue_data
 
-    def _digiball_parser(self, devices):
+    def _digiball_parser(self, device, advertising_data):
+        mac_address = device.address
+        d = advertising_data
+        rssi = d.rssi
+        manuf = d.manufacturer_data
+        for manuf_id in manuf:
 
-        for mac_address in devices:
-            d = devices[mac_address][1]
-            rssi = d.rssi
-            manuf = d.manufacturer_data
-            for manuf_id in manuf:
+            if manuf_id == 0x03DE: #NRLLC
 
-                if manuf_id == 0x03DE: #NRLLC
+                print("DigiBall detected")
 
-                    mdata = manuf[manuf_id]
-                    device_type = int(mdata[3]&0xF)
+                mdata = manuf[manuf_id]
+                device_type = int(mdata[3]&0xF)
 
-                    if device_type==1: #DigiBall device type is always 1
+                if device_type==1: #DigiBall device type is always 1
 
-                        # Save device as target if brought close to receiver
-                        rssi_range = -55
-                        if self._digiball_mac_addresses[0] is None and rssi>rssi_range:
-                            self._digiball_mac_addresses[0] = mac_address
-                        elif self._digiball_mac_addresses[1] is None and rssi>rssi_range and mac_address!=self._digiball_mac_addresses[0]:
-                            self._digiball_mac_addresses[1] = mac_address
+                    # Save device as target if brought close to receiver
+                    rssi_range = -55
+                    if self._digiball_mac_addresses[0] is None and rssi>rssi_range:
+                        self._digiball_mac_addresses[0] = mac_address
+                    elif self._digiball_mac_addresses[1] is None and rssi>rssi_range and mac_address!=self._digiball_mac_addresses[0]:
+                        self._digiball_mac_addresses[1] = mac_address
 
-                        if mac_address in self._digiball_mac_addresses:
+                    if mac_address in self._digiball_mac_addresses:
 
-                            if mac_address == self._digiball_mac_addresses[0]:
-                                player = 1
-                            else:
-                                player = 2
+                        if mac_address == self._digiball_mac_addresses[0]:
+                            player = 1
+                        else:
+                            player = 2
 
-                            data_ready = (int(mdata[17]) >> 6) == 1
-                            shot_number = int(mdata[6]) & 0x3F
+                        data_ready = (int(mdata[17]) >> 6) == 1
+                        shot_number = int(mdata[6]) & 0x3F
 
-                            if data_ready:
-                                data = {}
-                                data["RSSI"] = rssi
-                                data["MAC Address"] = mac_address
+                        if data_ready:
+                            data = {}
+                            data["RSSI"] = rssi
+                            data["MAC Address"] = mac_address
 
-                                #Parse digiball data here
-                                data["Charging"] = int(mdata[7])>>6
-                                data["Gyro Clipping"] = (int(mdata[6])>>7)==1
-                                data["Motionless"] = (int(mdata[7]) & 0x03) + int(mdata[8])
-                                data["Shot Number"] = shot_number
-                                data["Tip Percent"] = int(mdata[11])
-                                speed_factor = int(mdata[12])
-                                spin_horz_dps = struct.unpack('>h', mdata[13:15])[0]
-                                spin_vert_dps = struct.unpack('>h', mdata[15:17])[0]
-                                spin_mag_rpm = sqrt(spin_horz_dps ** 2 + spin_vert_dps ** 2) / 6
-                                data["Speed KMPH"] = 0.06 * 1.60934 * speed_factor
-                                spin_degrees = 180 / pi * atan2(spin_horz_dps, spin_vert_dps)
-                                data["Spin RPS"] = spin_mag_rpm/60
-                                data["Tip Angle"] = spin_degrees
+                            #Parse digiball data here
+                            data["Charging"] = int(mdata[7])>>6
+                            data["Gyro Clipping"] = (int(mdata[6])>>7)==1
+                            data["Motionless"] = (int(mdata[7]) & 0x03) + int(mdata[8])
+                            data["Shot Number"] = shot_number
+                            data["Tip Percent"] = int(mdata[11])
+                            speed_factor = int(mdata[12])
+                            spin_horz_dps = struct.unpack('>h', mdata[13:15])[0]
+                            spin_vert_dps = struct.unpack('>h', mdata[15:17])[0]
+                            spin_mag_rpm = sqrt(spin_horz_dps ** 2 + spin_vert_dps ** 2) / 6
+                            data["Speed KMPH"] = 0.06 * 1.60934 * speed_factor
+                            spin_degrees = 180 / pi * atan2(spin_horz_dps, spin_vert_dps)
+                            data["Spin RPS"] = spin_mag_rpm/60
+                            data["Tip Angle"] = spin_degrees
 
 
 
-                                ball_types = ((2.250, "White", 0.465, 0.358),  # Pool
-                                              (2.438, "White", 0.465, 0.358),  # Carom
-                                              (2.438, "Yellow", 0.465, 0.358),  # Carom yellow
-                                              (2.063, "White", 0.354, 0.250),  # Snooker
-                                              (2.000, "White", 0.315, 0.250),  # English pool
-                                              (2.688, "White", 0.492, 0.375))  # Russian pyramid
+                            ball_types = ((2.250, "White", 0.465, 0.358),  # Pool
+                                          (2.438, "White", 0.465, 0.358),  # Carom
+                                          (2.438, "Yellow", 0.465, 0.358),  # Carom yellow
+                                          (2.063, "White", 0.354, 0.250),  # Snooker
+                                          (2.000, "White", 0.315, 0.250),  # English pool
+                                          (2.688, "White", 0.492, 0.375))  # Russian pyramid
 
-                                ball_type = (mdata[3] >> 4) & 0xF
-                                if ball_type>(len(ball_types)-1):
-                                    ball_type = 0
+                            ball_type = (mdata[3] >> 4) & 0xF
+                            if ball_type>(len(ball_types)-1):
+                                ball_type = 0
 
-                                properties = ball_types[ball_type]
+                            properties = ball_types[ball_type]
 
-                                data["Ball Diameter"] = properties[0]
-                                data["Ball Color"] = properties[1]
-                                data["Tip Diameter"] = properties[2]
-                                data["Tip Curvature"] = properties[3]
+                            data["Ball Diameter"] = properties[0]
+                            data["Ball Color"] = properties[1]
+                            data["Tip Diameter"] = properties[2]
+                            data["Tip Curvature"] = properties[3]
 
-                                #Post data
-                                if self._digiball_player_data[player-1] is None:
-                                    self._new_device = True
-                                self._digiball_player_data[player-1] = data
+                            #Post data
+                            if self._digiball_player_data[player-1] is None:
+                                self._new_device = True
+                            self._digiball_player_data[player-1] = data
 
 
     def _digicue_parser(self, devices):
@@ -274,10 +274,10 @@ class BLE_async():
         stop_event = asyncio.Event()
 
         def callback(device, advertising_data):
-            print(device, advertising_data)
-            # self._digiball_parser(self._devices)
-            # self._digicue_parser(self._devices)
-            # q.put((self._digiball_player_data, self._digicue_player_data))
+            #Figure out how to get data to these functions
+            self._digiball_parser(device, advertising_data)
+            #self._digicue_parser(advertising_data)
+            #q.put((self._digiball_player_data, self._digicue_player_data))
 
         async with BleakScanner(callback) as scanner:
             # Important! Wait for an event to trigger stop, otherwise scanner
